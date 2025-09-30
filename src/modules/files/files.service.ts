c04731d7f6e55f406f56136704a9f8be6b24c1ac
HaCaoVi <icaovy2001@gmail.com>
2025-09-30 16:46:51 +0700

    update delete image

--- src/modules/files/files.service.ts
+++ src/modules/files/files.service.ts
@@ -2,24 +2,32 @@
 import { Injectable, Inject } from '@nestjs/common';
 import * as admin from 'firebase-admin';
 import { v4 as uuidv4 } from 'uuid';
+import { Bucket } from '@google-cloud/storage';
 
 @Injectable()
 export class FilesService {
-  constructor(@Inject('FIREBASE_APP') private readonly firebaseApp: admin.app.App) { }
+  private bucket: Bucket;
+  constructor(@Inject('FIREBASE_APP') private readonly firebaseApp: admin.app.App) {
+    this.bucket = this.firebaseApp.storage().bucket();
+  }
+
+  async deleteFile(oldFileName: string) {
+    try {
+      const oldFileRef = this.bucket.file(oldFileName);
+      await oldFileRef.delete();
+      console.log(`Deleted old file: ${oldFileName}`);
+    } catch (err) {
+      console.warn(`Failed to delete old file: ${oldFileName}`, err);
+    }
+  }
 
   async uploadImage(file: Express.Multer.File, folderName: string, oldFileName: string) {
-    const bucket = this.firebaseApp.storage().bucket();
+    // const bucket = this.firebaseApp.storage().bucket();
     const filename = `files/images/${folderName}/${uuidv4()}-${Date.now()}-${file.originalname}`;
-    const fileRef = bucket.file(filename);
+    const fileRef = this.bucket.file(filename);
 
     if (oldFileName || oldFileName.length > 0) {
-      const oldFileRef = bucket.file(oldFileName);
-      try {
-        await oldFileRef.delete();
-        console.log(`Deleted old file: ${oldFileName}`);
-      } catch (err) {
-        console.warn(`Failed to delete old file: ${oldFileName}`, err);
-      }
+      this.deleteFile(oldFileName)
     }
 
 
@@ -30,23 +38,16 @@ export class FilesService {
 
     return {
       fileName: filename,
-      url: `https://storage.googleapis.com/${bucket.name}/${filename}`,
+      url: `https://storage.googleapis.com/${this.bucket.name}/${filename}`,
     };
   }
 
   async uploadVideo(file: Express.Multer.File, folderName: string, oldFileName: string) {
-    const bucket = this.firebaseApp.storage().bucket();
     const filename = `files/videos/${folderName}/${uuidv4()}-${Date.now()}-${file.originalname}`;
-    const fileRef = bucket.file(filename);
+    const fileRef = this.bucket.file(filename);
 
     if (oldFileName || oldFileName.length > 0) {
-      const oldFileRef = bucket.file(oldFileName);
-      try {
-        await oldFileRef.delete();
-        console.log(`Deleted old file: ${oldFileName}`);
-      } catch (err) {
-        console.warn(`Failed to delete old file: ${oldFileName}`, err);
-      }
+      this.deleteFile(oldFileName)
     }
 
     await fileRef.save(file.buffer, {
@@ -56,7 +57,7 @@ export class FilesService {
 
     return {
       fileName: filename,
-      url: `https://storage.googleapis.com/${bucket.name}/${filename}`,
+      url: `https://storage.googleapis.com/${this.bucket.name}/${filename}`,
     };
   }
 }
